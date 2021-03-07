0f558020db86a681f43f5cf96d747d59a51cb6ef
Kaustav Ghosh <teetangh@gmail.com>
2021-03-08 04:05:20 +0530

    added logistic regression

--- ML/logistic_regression.py
+++ ML/logistic_regression.py
@@ -1,25 +1,78 @@
-# WORK IN PROGRESS
-
 import numpy as np
+import math
+import random 
 
+def compute_z(theta , x):
+    z = 0
+    for j in range(len(x)):
+        z += theta[j] * x[j]
+    z += theta[len(x)]
+    return z
+
+def compute_g(z):
+    return (1)/(1 + math.exp(-z))
+
+def compute_h(z):
+    return compute_g(z)
+
+def binary_cross_entropy_loss(Y_train , Y_predict):
+    
+    total = 0
+    for i in range(len(Y_train)):
+        total -= (Y_train[i] * math.log(Y_predict[i])) + ((1 - Y_train[i]) * math.log(1-Y_predict[i]))
+    
+    average = total / len(Y_train)
+    return average
+
+def compute_loss_gradients(theta , X_train , Y_train , Y_predict):
+    delta_theta = []
+
+    for j in range(len(X_train[0])):
+        grad = 0
+        for i in range(len(Y_train)):
+            grad += ((Y_predict[i] - Y_train[i]) * X_train[i][j])/len(Y_train)
+            
+        delta_theta.append(grad)
+
+    return delta_theta
 
 def main():
-    m = int(input("Enter m,the number of input samples"))
+    # f = int(input("no of features: "))
+    n = int(input("no of rows: "))
 
+    X_train = []
+    Y_train = []
     for i in range(n):
-        txt = "Enter the {value}th example in the training set."
-        xi, yi = input(txt.format(value=i)).split()
+        row = [ int(r) for r in input().split() ]
 
-        X.append(int(xi))
-        Y.append(int(yi))
+        X_train.append(row[0:-1])
+        Y_train.append(row[-1])
+    
+    theta = [ np.random.randn() for i in range(len(X_train))]
+    print("theta",theta)
+    for i in range(n):
+        print(X_train[i] , Y_train[i])
 
-    for j in range(0, m):
-        theta[j] = 0
-        gradient[j] = 0
+    epochs = 5
+    epsilon = 0.00000000000000001
+    alpha = 0.001
+    for e in range(epochs):
+        Y_predict = [ ]
+        for i in range(n):
+            print(X_train[i])
+            Y_predict.append(compute_h(compute_z(theta , X_train[i])))
 
-    for i in range(0, m):
-        pass
-
+        current_loss = binary_cross_entropy_loss(Y_train,Y_predict)
+        print("=========> Epoch number:",e, "Current Loss: " , current_loss)
+        print("Y_predict",Y_predict)
+        if current_loss <= epsilon:
+              break
+        
+        delta_theta = compute_loss_gradients(theta , X_train  ,Y_train , Y_predict )
+        print("delta_theta", delta_theta)
+        for j in range(len(theta) - 1):
+            theta[j] = theta[j] - alpha * delta_theta[j] 
 
 if __name__ == "__main__":
     main()
+
